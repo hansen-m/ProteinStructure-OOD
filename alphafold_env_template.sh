28be3ec23a99faf8e8164da461dd6f19791b2db3
mah60 <mah60@psu.edu>
2024-11-25 20:39:57 -0500

    Updated alphafold env

--- alphafold_env_template.sh
+++ alphafold_env_template.sh
@@ -1,27 +1,31 @@
+#!/bin/bash
+
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 
-ALPHAFOLD_BASE="/path/to/alphafold"
-ALPHAFOLD_DB="${ALPHAFOLD_BASE}/alphafold_2.3_db"
+export ALPHAFOLD_BASE="/path/to/alphafold"
+export ALPHAFOLD_DB="${ALPHAFOLD_BASE}/alphafold_2.3_db"
 
-CURRENT_DATE=$(date +"%Y%m%d_%H%M%S")
-RUN_DIR="${WORKINGDIR}/run_${CURRENT_DATE}"
+export CURRENT_DATE=$(date +"%Y%m%d_%H%M%S")
+export RUN_DIR="${WORKINGDIR}/run_${CURRENT_DATE}"
 
-CPU_OUTPUT="${RUN_DIR}/CPU-SLURM"
-GPU_OUTPUT="${RUN_DIR}/GPU-SLURM"
-STRUCT="${RUN_DIR}/DESIGN-ESM"
-LOGDIR="${RUN_DIR}/logs"
+export CPU_OUTPUT="${RUN_DIR}/CPU-SLURM"
+export GPU_OUTPUT="${RUN_DIR}/GPU-SLURM"
+export STRUCT="${RUN_DIR}/DESIGN-ESM"
+export LOGDIR="${RUN_DIR}/logs"
 
-UNIREF90_PATH="${ALPHAFOLD_DB}/uniref90/uniref90.fasta"
-MGNIFY_PATH="${ALPHAFOLD_DB}/mgnify/mgy_clusters_2022_05.fa"
-TEMPLATE_MMCIF_PATH="${ALPHAFOLD_DB}/pdb_mmcif/mmcif_files"
-OBSOLETE_PDBS_PATH="${ALPHAFOLD_DB}/pdb_mmcif/obsolete.dat"
-UNIPROT_PATH="${ALPHAFOLD_DB}/uniprot/uniprot.fasta"
-PDB_SEQRES_PATH="${ALPHAFOLD_DB}/pdb_seqres/pdb_seqres.txt"
-UNIREF30_PATH="${ALPHAFOLD_DB}/uniref30/UniRef30_2021_03"
-BFD_PATH="${ALPHAFOLD_DB}/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt"
+export UNIREF90_PATH="${ALPHAFOLD_DB}/uniref90/uniref90.fasta"
+export MGNIFY_PATH="${ALPHAFOLD_DB}/mgnify/mgy_clusters_2022_05.fa"
+export TEMPLATE_MMCIF_PATH="${ALPHAFOLD_DB}/pdb_mmcif/mmcif_files"
+export OBSOLETE_PDBS_PATH="${ALPHAFOLD_DB}/pdb_mmcif/obsolete.dat"
+export UNIPROT_PATH="${ALPHAFOLD_DB}/uniprot/uniprot.fasta"
+export PDB_SEQRES_PATH="${ALPHAFOLD_DB}/pdb_seqres/pdb_seqres.txt"
+export UNIREF30_PATH="${ALPHAFOLD_DB}/uniref30/UniRef30_2021_03"
+export BFD_PATH="${ALPHAFOLD_DB}/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt"
 
-ALPHAFOLD_CONTAINER="${ALPHAFOLD_BASE}/singularity/alphafold_2.3.2-1.sif"
-ALPHAFOLD_GPU_SCRIPT="${ALPHAFOLD_BASE}/scripts/run/run_alphafold-gpu_2.3.2.py"
+export ALPHAFOLD_CONTAINER="${ALPHAFOLD_BASE}/singularity/alphafold_2.3.2-1.sif"
+export ALPHAFOLD_GPU_SCRIPT="${ALPHAFOLD_BASE}/scripts/run/run_alphafold-gpu_2.3.2.py"
 
-JOB_NAME="alphafold_job_${USER}"
-FASTA_FILE="${CPU_OUTPUT}/input.fasta"
+export HHBLITS_BINARY_PATH="${ALPHAFOLD_BASE}/hh-suite/bin/hhblits"
+
+export JOB_NAME="alphafold_job_${USER}"
+export FASTA_FILE="${CPU_OUTPUT}/input.fasta"
